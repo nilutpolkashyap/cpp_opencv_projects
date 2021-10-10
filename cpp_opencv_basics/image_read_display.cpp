9ca7d62b3bb620d8aa361b43a86dfcf52a959ff4
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2021-10-11 04:45:24 +0530

    code for image read and display

---
+++ cpp_opencv_basics/image_read_display.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+    cout << "OpenCV Image Read & Display using C++" << endl;
+
+    Mat img = imread("lena.jpg", 1);
+
+    imshow("image", img);
+    waitKey(0);
+
+    destroyAllWindows();
+
+    return 0;
+}
