c71c79ff4a669caf61f6e1313f0d8c1813447547
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2021-10-25 19:31:00 +0530

    image read and display

--- cpp_opencv_basics/image_read_display.cpp
+++ cpp_opencv_basics/image_read_display.cpp
@@ -8,36 +8,25 @@ int main(int argc, char **argv)
 {    
     cout << "OpenCV Image Read & Display using C++" << endl;
 
-    Mat img = imread("lena.jpg", 1);
+    // Mat - n-dimensional array class for images using opencv
+    Mat image = imread("lena.jpg", IMREAD_COLOR);
+    // 0 - IMREAD_GRAYSCALE, 1 - IMREAD_COLOR, -1 - IMREAD_UNCHANGED
+
+    Mat gray_image;
+
+    //convert BGR to Gray Scale image
+    cvtColor(image, gray_image, COLOR_BGR2GRAY);
 
     // cout << img << endl;
 
-    imshow("image", img);
-    waitKey(0);
+    imshow("gray image", gray_image);
+    imshow("image", image);
+    waitKey(5000);
+    // 1 second = 1000
 
-    destroyAllWindows();
+    // destroyAllWindows();
+    destroyWindow("image");
 
     return 0;
 }
 
-// #include <iostream>
-// #include <opencv2/opencv.hpp>
-
-// using namespace std;
-// using namespace cv;
-
-// int main(int argc, char **argv)
-// {
-//     cout << "****************************************************" << endl;
-//     cout << "Using Opencv C++ to read and display image" << endl;
-
-//     Mat img = imread("lena.jpg", 0);
-
-//     imshow("image ", img);
-
-//     waitKey(0);
-
-//     destroyAllWindows();
-
-//     return 0;
-// }
