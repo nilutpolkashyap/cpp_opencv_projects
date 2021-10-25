61448bfe06b57a4271fa012941ec2920f73e4aa7
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2021-10-26 01:09:07 +0530

    update

---
+++ cpp_opencv_basics/image_resizing.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+    Mat image;
+
+    image = imread("lenaa.jpg",1);
+
+    
+}
