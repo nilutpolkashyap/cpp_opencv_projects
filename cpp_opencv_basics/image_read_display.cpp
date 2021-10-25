39e16d1045c572a7f6f3361b21fea83004345501
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2021-10-25 09:40:34 +0530

    commit

--- cpp_opencv_basics/image_read_display.cpp
+++ cpp_opencv_basics/image_read_display.cpp
@@ -5,7 +5,7 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char **argv)
-{
+{    
     cout << "OpenCV Image Read & Display using C++" << endl;
 
     Mat img = imread("lena.jpg", 1);
@@ -19,3 +19,25 @@ int main(int argc, char **argv)
 
     return 0;
 }
+
+// #include <iostream>
+// #include <opencv2/opencv.hpp>
+
+// using namespace std;
+// using namespace cv;
+
+// int main(int argc, char **argv)
+// {
+//     cout << "****************************************************" << endl;
+//     cout << "Using Opencv C++ to read and display image" << endl;
+
+//     Mat img = imread("lena.jpg", 0);
+
+//     imshow("image ", img);
+
+//     waitKey(0);
+
+//     destroyAllWindows();
+
+//     return 0;
+// }
