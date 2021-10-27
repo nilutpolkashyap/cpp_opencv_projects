76669f472c8fbcd661a0a6b83d4c41d94a18c636
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2021-10-28 03:39:45 +0530

    update

---
+++ cpp_opencv_basics/image_rotation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+    cout << "Code for Image Rotation " << endl;
+    
+
+    return 0;
+}
