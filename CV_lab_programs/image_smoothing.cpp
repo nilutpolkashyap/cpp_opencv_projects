c56aa51fe084ae5c892ad216aa63befb735a9c83
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2021-12-06 18:42:25 +0530

    code for image histogram and image blur

---
+++ CV_lab_programs/image_smoothing.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+    Mat image, blurred;
+    image = imread("wood.jpg", cv::IMREAD_COLOR);
+
+    blur(image, blurred, Size(5,5));
+
+    imshow("original_image", image); 
+    imshow("blurred_image", blurred); 
+    waitKey(0);
+    destroyAllWindows();
+
+    return 0;
+}
