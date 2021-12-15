054417c8f12409f8625acbf3621cea439b268a2e
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2021-12-15 23:56:17 +0530

    code for image alpha blending

---
+++ image_blending/image_blending.cpp
@@ -0,0 +1,22 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+Mat img1, img2, output;
+
+int main(int argc, char **argv)
+{
+    img1 = imread("image1.jpg", IMREAD_COLOR);
+    img2 = imread("image2.jpg", IMREAD_COLOR);
+
+    float alpha, beta;
+    cout << "Enter value of Alpha for alpha blending [0.0 - 1.0] : " ;
+    cin >> alpha;
+    
+    beta = (1.0 - alpha);
+
+    cv:addWeighted(img1, alpha, img2, beta, 0.0, output);
+    
+}
